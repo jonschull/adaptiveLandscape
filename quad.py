b82b760e47dc96295e6401a8772053b9eb52a176
jonschull <Jschull@gmail.com>
2020-08-15 15:24:40 -0400

    now a dict. and passes test2

--- quad.py
+++ quad.py
@@ -6,7 +6,10 @@ green=color.green
 blue=color.blue
 yellow=color.yellow
 black=color.black
-gray=vec(0.2,0.2,0.2)
+gray=vec(0.5,0.5,0.5) #gray is broken in color
+
+numRows=4
+numCols=4 #NOTE makePatches will create marginal rows at -1 and num+1
 
 def showQuad():
     """how to make a quad"""
@@ -28,66 +31,55 @@ def showVecs():
 def square(row=0, col=0, alt=0, color=blue):
     """make a flat square"""
     r,c,a = -row, col, alt
+    
     vs = [  vertex(pos = vec(c,  r,   a), color=red),
             vertex(pos = vec(c,  r+1, a), color=blue),
             vertex(pos = vec(c+1,r+1, a), color=green),
             vertex(pos = vec(c+1,r,   a), color=yellow)  ]
     
-#    vs = [  vertex(pos = vec(c,  r,   a), color=gray),
-#            vertex(pos = vec(c,  r+1, a), color=gray),
-#            vertex(pos = vec(c+1,r+1, a), color=gray),
-#            vertex(pos = vec(c+1,r,   a), color=gray)  ]
-
     return quad(vs=vs)
 
-def makePatches(numRows=5, numCols=5):
-    
-    patches=[]
-    numRows=numCols=5
-    for row in range(-1, numRows + 1): # 1,-1 for margin row
-        R=[]
-        for col in range(-1, numCols +1): #-1, 1 for margin col
-            R.append(square(row=row,col=col))
-        patches.append(R)
+def makePatches(numRows=4, numCols=4):
+    patches={}
+    for row in range(-1, numRows+1): 
+        for col in range(-1, numCols+1):
+            patches[row,col] = square(row=row,col=col)
     return patches
 
-patches = makePatches()    
+patches = makePatches()
+
+def colorPatch(row=0,col=0, color=gray):
+    patch=patches[row,col]    
+    for i,v in enumerate(patch.vs):
+        patch.vs[i].color = gray
+
+def grayMargins():
+    for row in [-1,numRows]: 
+        for col in range(-1,numCols+1):
+            colorPatch(row,col)
+            
+    for col in [-1,numRows]: 
+        for row in range(-1,numRows+1):
+            colorPatch(row,col)
 
 
-def info(patches=patches):
-    rows = len(patches)
-    cols = len(patches[0])
-    lastrow = len(patches) - 1
-    lastcol= len(patches[lastrow]) -1
-    
-    if lastcol<cols: #time to move to next row
-        nextcell = (lastrow, lastcol+1)
-    else:
-        nextcell = (lastrow+1, 0)
-    
-    return dict(rows=rows,
-                cols=cols,
-                lastrow=lastrow,
-                lastcol=lastcol,
-                nextcell=nextcell)
-
-
-print(info(patches))
 
 from copy import copy, deepcopy
 
 def changePatch(row=1,col=1, alt=0.5):
-    target = patches[row][col]
-    target.v0.pos = patches[row-1][col].v3.pos;   
-    target.v1.pos = patches[row-1][col-1].v3.pos; 
-    target.v2.pos = patches[row][col-1].v3.pos;   
+    target = patches[row,col]
+    target.v0.pos = patches[row-1, col].v3.pos;   
+    target.v1.pos = patches[row-1, col-1].v3.pos; 
+    target.v2.pos = patches[row,   col-1].v3.pos;   
     target.v3.pos = target.v3.pos;                
     target.v3.pos.z = alt
     target.v0.color = color.white 
     target.v1.color = color.orange
     target.v2.color = color.magenta 
     target.v3.color = vec(0.5,0.5,0.5) #gray is broken
-    patches[row][col]=target
+    patches[row,col]=target
+
+#changePatch()
 
 def test1():
     changePatch(1,1,0.5)
@@ -99,12 +91,12 @@ def test1():
 
 #test1()
 
-rows, cols, lastrow, lastcol, nextcell = (info().values())
-
 def test2():
 
     i=0.0
-    for row in range(rows):
-        for col in range(cols-1):
+    for row in range(numRows):
+        for col in range(numCols):
             i = i + 0.3
             changePatch(row,col,i)
+            
+test2()
