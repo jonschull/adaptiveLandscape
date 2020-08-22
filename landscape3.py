cff8b15b2f87bed891d8a20c8d6d835b3dba84b9
jonschull <Jschull@gmail.com>
2020-08-22 11:51:37 -0400

    colors good within height = -2, +2

--- landscape3.py
+++ landscape3.py
@@ -1,36 +1,17 @@
-from vpython import compound, quad,color,vertex,vec, label, scene
-
-def Quad():
-    """how to make a quad"""
-    a = vertex( pos=vec(0,0,0),color=color.red )
-    b = vertex( pos=vec(1,0,0),color=color.blue )
-    c = vertex( pos=vec(1,1,0),color=color.green )
-    d = vertex( pos=vec(0,1,0),color=color.yellow )
-    return quad( vs=[a,b,c,d] )
-
+from vpython import *
 
 Rows = 3
 Cols = 3
 scene.center = vec(Rows, Cols, 0)
-#scene.width = scene.height = 800
+local_light(pos=vec(100,100,0), color=color.red)
+local_light(pos=vec(-100,-100, 0), color=color.blue)
 
 #make vertices
 vertices = dict()
 for row in range(-1, 2*Rows+1):
     for col in range(-1, 2*Cols+1):
-        vertices[row,col] = vertex(pos=vec(row,col,-0))
-
-#make labels from vertices
-# labels=[]
-# for k,v in vertices.items():
-#     row,col = k
-#     labels.append(
-#         label(pos= vertices[row,col].pos, 
-#                text=str(k), 
-#                box = False,
-#                opacity=0 )
-#     )
-
+        vertices[row,col] = vertex(pos=vec(row,col,0))
+        #label(border=0, opacity=0, pos=vec(row,col,0), text=str(vec(row,col,0)))
 
 # make quads 
 quads=dict()
@@ -42,19 +23,30 @@ for row in range(2*Rows+1):
                 vertices[row-1,col],
                 vertices[row-1,col-1],
                 vertices[row, col-1]
-            ])
+            ], 
+            texture=textures.rough,
+            shininess=0)
         if row%2 and col%2:
-            quads[row,col].v0.color=color.blue
-            quads[row,col].v1.color=color.blue
-            quads[row,col].v2.color=color.blue
-            quads[row,col].v3.color=color.blue
             cells.append(quads[row,col])
 
-def modCell(row=0,col=0, height=1):
-    target = cells[0]
+def rowcol(row=0,col=0):
+    "integer corresonding to row,col"
+    return row * Cols + col
+
+def modCell(row=0,col=0, height=0.5,color=vec(1,1,1)):
+    target = cells[rowcol(row,col)]
     for i,v in enumerate(target.vs):
         target.vs[i].pos.z = height
-        target.vs[i].color = vec(0,height, 0)
-        
-modCell()
+        target.vs[i].color = color
+        target.emissive=False
 
+#test
+def test():
+    for row in range(Rows):
+        print()
+        for col in range(Cols):
+            height=(row - col)/((Rows+Cols)/2)
+            color=vec(-height,0.3 + height, height)
+            modCell(row,col, height = height, color=color)
+            print(height, end=' ')
+test()
