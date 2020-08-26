01a6085d952a934740b94062e5ade802d4cd9abd
jonschull <Jschull@gmail.com>
2020-08-26 19:55:11 -0400

    refactored with "units" that know how to change themselves

--- landscape3.py
+++ landscape3.py
@@ -8,16 +8,28 @@ local_light(pos=vec(-100,-100, 0), color=color.blue)
 
 #make vertices
 vertices = dict()
-
 def positionVertices():
     for row in range(-1, 2*Rows+1):
         for col in range(-1, 2*Cols+1):
             vertices[row,col] = vertex(pos=vec(row,col,0), initialPos = vec(row,col,0))
 positionVertices()
 
-# make quads 
-quads=dict()
-cells=[]
+
+def avg(lst):
+    return sum(lst)/len(lst)
+
+def pos(cell):
+    x = avg( [v.pos.x for v in cell.vs] )
+    y = avg( [v.pos.y for v in cell.vs] )
+    z = avg( [v.pos.z for v in cell.vs] )
+    return vec(x,y,z)
+
+
+# make quads and cells and boxes
+quads   = dict()
+cellDict = dict()
+boxes = dict()
+
 for row in range(2*Rows+1):
     for col in range( 2*Cols+1):
         quads[row,col] = quad(vs=[
@@ -29,33 +41,63 @@ for row in range(2*Rows+1):
             texture=textures.rough,
             shininess=0)
         if row%2 and col%2:
-            cells.append(quads[row,col])
-
-def rowcol(row=0,col=0):
-    "integer corresonding to row,col"
-    return row * Cols + col
+            cellDict[row//2, col//2] = quads[row,col]
+            cellDict[row//2, col//2].visible=False
+            boxes[row//2, col//2] = box(
+                                        pos = pos(quads[row,col]),
+                                        size= vec(0.9,0.9,0.1)
+                                        )
 
 def cell(row,col):
-    return cells[rowcol(row,col)]
+    """return the cell corresponding to row,col"""
+    return cellDict[row,col]
 
-def heightAndColor(row=0,col=0, height=999,color=None):
-    target = cell(row,col)
-    for i,v in enumerate(target.vs):
-        if height!=999:
-            target.vs[i].pos.z = height
+def colorFunc(height):
+    return vec(0.3-height,0.3 + height, 0.3-height)
+
+# create a Unit class with a smart change method
+# (use change to change pos, height, etc., not pos= as with vpython objects)
+class Unit:
+    def __init__(self, row,col):
+        self.box=boxes[row,col]
+        self.cell = cell(row,col)
+        self.pos = self.box.pos
+
+    def change(self,color=None, pos=None, height=None):
         if color:
-            target.vs[i].color = color
-    target.visible=False
-    target.cellHeight = height
-    target.cellColor = color
+            self.box.color=color
+            for i in range(4):
+                self.cell.vs[i].color=color
+            
+        if pos:
+            oldPos = self.pos
+            deltaPos = oldPos - pos
+            self.pos = pos
+            self.box.pos = pos
+            for i in range(4):
+                self.cell.vs[i].pos -= deltaPos
+
+        if height:
+            newPos = vec(
+                self.pos.x,
+                self.pos.y,
+                height)
+            newColor = colorFunc(height)
+            self.change( pos=newPos, color =newColor )
+            
+#make the units
+units=dict()
+for row in range(Rows):
+    for col in range(Cols):
+        units[row,col]= Unit(row,col)
 
 def initLandscape():
     for row in range(Rows):
-        print()
         for col in range(Cols):
             height=(col - row)/((Rows+Cols)/3)
-            color=vec(0.3-height,0.3 + height, 0.3-height)
-            heightAndColor(row,col, height = height, color=color)
+            color=colorFunc(height)
+            units[row,col].change(height=height,color=color) #use the changemethod
+
 initLandscape()
 
 def resetVertices():
@@ -63,14 +105,6 @@ def resetVertices():
         vertices[v].pos = vertices[v].initialPos
         initLandscape()
 
-def moveCells(deltaX=0,deltaY=0):
-    for row in range(Rows):
-        for col in range(Cols):
-            target = cell(row,col)
-            for i in range(4):
-                target.vs[i].pos.x += deltaX
-                target.vs[i].pos.y += deltaY
-#moveCells(2,2)
 
 def wideAndLong(row=0,col=0, wide=0.5, long=0.5):
     target = cell(row,col)
@@ -90,27 +124,22 @@ def shrinkCells(wide=0.5,tall=0.5):
         for col in range(Cols):
             wideAndLong(row,col, wide,tall)
 
-def avg(lst):
-    return sum(lst)/len(lst)
 
-def pos(cell):
-    x = avg( [v.pos.x for v in cell.vs] )
-    y = avg( [v.pos.y for v in cell.vs] )
-    z = avg(  [v.pos.z for v in cell.vs] )
-    return vec(x,y,z)
+scene.caption="""
+<h3>caption</h3>
+Each column of squares represents a group. 
+Each row of squares represents a question.
+For each square<ul>
+<li> The altitude/color of each square reflects agreement.
+<li> The size of each square represents the number of people who voted.
+<li> The thickness of the black outline of each square represents the number of people who passed.
+</ul>
 
-agreeboxes=dict()
-def makeAgreeBoxes():
-    for row in range(Rows):
-        for col in range(Cols):
-            c= cell(row,col)
-            agreeboxes[row,col]=box(
-                pos=pos(c),
-                color=c.cellColor,
-                height=0.5,
-                length=0.5,
-                width=0.1
-            )
+This where detail would be shown on hover.
+<ul><li>question
+<li>Agree
+<li>Disagree
+<li>Pass
+</ul>
 
-
-makeAgreeBoxes()    
+"""
